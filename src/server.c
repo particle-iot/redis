2e582056bd79a62902f8e50c9804167df8bb7aa5
Julien Vanier <julien@particle.io>
2022-10-25 15:46:19 -0400

    Add a radix tree to the server to track pubsub prefix patterns

--- src/server.c
+++ src/server.c
@@ -2646,6 +2646,7 @@ void initServer(void) {
     evictionPoolAlloc(); /* Initialize the LRU keys pool. */
     server.pubsub_channels = dictCreate(&keylistDictType);
     server.pubsub_patterns = dictCreate(&keylistDictType);
+    server.pubsub_prefixes = raxNew();
     server.pubsubshard_channels = dictCreate(&keylistDictType);
     server.cronloops = 0;
     server.in_exec = 0;
@@ -5962,7 +5963,7 @@ sds genRedisInfoString(dict *section_dict, int all_sections, int everything) {
             server.stat_keyspace_hits,
             server.stat_keyspace_misses,
             dictSize(server.pubsub_channels),
-            dictSize(server.pubsub_patterns),
+            dictSize(server.pubsub_patterns) /* TODO: add number of patterns in pubsub_prefixes */,
             dictSize(server.pubsubshard_channels),
             server.stat_fork_time,
             server.stat_total_forks,
